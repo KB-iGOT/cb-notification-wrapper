5049095b9a10a458942f928d7810550074c6dc76
DeepikaMalav28 <deepika.malav@tarento.com>
2025-06-09 22:09:34 +0530

    kafka config fix

--- src/main/java/com/igot/cb/config/ProducerConfiguration.java
+++ src/main/java/com/igot/cb/config/ProducerConfiguration.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -15,7 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-
+@Configuration
 public class ProducerConfiguration {
 
     @Value("${spring.kafka.bootstrap.servers}")
