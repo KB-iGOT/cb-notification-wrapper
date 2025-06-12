8882c7d23342050912cc0d0e3a6d9538460d2db8
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-06-12 20:03:15 +0530

    fix for notification (#13) (#14)
    Co-authored-by: DeepikaMalav28 <deepika.malav@tarento.com>

--- src/main/java/com/igot/cb/notification/enums/NotificationSubCategory.java
+++ src/main/java/com/igot/cb/notification/enums/NotificationSubCategory.java
@@ -1,14 +1,14 @@
 package com.igot.cb.notification.enums;
 
 public enum NotificationSubCategory {
-    CONTENT_REVIEW_REQUEST(NotificationCategory.LEARN, "New content '{title}' has been submitted for your review"),
-    CONTENT_PUBLISHED(NotificationCategory.LEARN, "Your content '{title}' has been published successfully."),
-    CONTENT_REJECTED(NotificationCategory.LEARN, "Your content '{title}' was not approved. Please check reviewer comments."),
-    CONTENT_EDITED(NotificationCategory.LEARN,"Your content '{title}' was edited by the publisher. Review the changes."),
+    CONTENT_REVIEW_REQUEST(NotificationCategory.LEARN, "New content {title} has been submitted for your review"),
+    CONTENT_PUBLISHED(NotificationCategory.LEARN, "Your content {title} has been published successfully."),
+    CONTENT_REJECTED(NotificationCategory.LEARN, "Your content {title} was not approved. Please check reviewer comments."),
+    CONTENT_EDITED(NotificationCategory.LEARN,"Your content {title} was edited by the publisher. Review the changes."),
     LIKED_POST(NotificationCategory.DISCUSSION, "{userName} liked your post."),
     LIKED_COMMENT(NotificationCategory.DISCUSSION, "{userName} commented on your post."),
     REPLIED_POST(NotificationCategory.DISCUSSION, "{userName} liked your reply."),
-    POST_COMMENT(NotificationCategory.DISCUSSION ,"username liked on your commented post."),
+    POST_COMMENT(NotificationCategory.DISCUSSION ,"userName liked on your commented post."),
     REPLIED_COMMENT(NotificationCategory.DISCUSSION,"{userName} replied to your comment."),
     SEND_CONNECTION_REQUEST(NotificationCategory.NETWORK, "{userName} has sent you a connection request."),
     ACCEPTED_CONNECTION_REQUEST(NotificationCategory.NETWORK, "{userName} accepted your connection request."),
