166f31ae22c9b2625e387df65750e1129e23a086
DeepikaMalav28 <deepika.malav@tarento.com>
2025-08-12 12:22:36 +0530

    KB-10615 remove unused code

--- src/main/java/com/igot/cb/notification/enums/UserRole.java
+++ src/main/java/com/igot/cb/notification/enums/UserRole.java
@@ -1,11 +1,5 @@
 package com.igot.cb.notification.enums;
 
 public enum UserRole {
-    CBP_ADMIN,
-    CONTENT_CREATOR,
-    MDO_ADMIN,
-    MDO_REPORT_ACCESSOR,
-    PUBLIC,
-    SPV_ADMIN,
-    SPV_PUBLISHER
+    CBP_ADMIN
 }
