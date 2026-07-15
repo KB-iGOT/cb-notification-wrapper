f0d3cd95e0dd8b90911e4973a6287c5731d4d70d
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2026-07-15 19:45:19 +0530

    4.8.39.1 kb 14441 (#76)
    * KB-14441 implemented notification functionality for re-enroll and un-enroll (#74)
    
    * KB-14441 implemented notification functionality for re-enroll and un-enroll
    
    * KB-14441 implemented notification functionality for re-enroll and un-enroll
    
    * KB-14441 implemented notification functionality for re-enroll and un-enroll
    
    * Merge pull request #75 from pathiktarento1089/4.8.39.1-KB-14441
    
    KB-14441 updating  notification subCategory for re-enroll and un-enroll.
    
    ---------
    
    Co-authored-by: pathiktarento1089 <123535830+pathiktarento1089@users.noreply.github.com>

--- src/main/java/com/igot/cb/notification/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/igot/cb/notification/service/impl/NotificationServiceImpl.java
@@ -207,7 +207,9 @@ public class NotificationServiceImpl implements NotificationService {
                 NotificationSubCategory.CONTENT_RETIRED,
                 NotificationSubCategory.RETIRE_REJECTED,
                 NotificationSubCategory.RETIRE_APPROVED,
-                NotificationSubCategory.EXTERNAL_TRAINING
+                NotificationSubCategory.EXTERNAL_TRAINING,
+                NotificationSubCategory.CONTENT_UN_ENROLLED,
+                NotificationSubCategory.CONTENT_RE_ENROLLED
         ).contains(subCategory);
     }
 
